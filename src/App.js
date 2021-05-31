066706734d39b840f804b68d02788635cee15a2a
Marc Nopprapun <mnopprapun@gmail.com>
2021-05-31 11:06:07 -0500

    second push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import Game from 'src/Comonents/Game.js';
+import Board from 'src/Comonents/Board.js'
 import './App.css';
+import { render } from '@testing-library/react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  fetch("http://localhost:9292/Users")
+  .then((res) => res.json())
+  .then(console.log); 
+
+  function Square(props) {
+    return ( 
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+  render()
+  return(
+    <Game>
+
+    </Game>
+  )
+
+}
 export default App;
